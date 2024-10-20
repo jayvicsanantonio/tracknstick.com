8eddef68c97122d7fd739ed151f8dfb89ad054db
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-10-19 23:40:47 -0700

    Add <Welcome />

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Header from "@/components/common/Header";
-import Footer from "@/components/Footer";
+import Welcome from "./components/common/Welcome";
+import Footer from "@/components/common/Footer";
 import { useToggle } from "./hooks/use-toggle";
 
 function App() {
+  const [habits] = useState([]);
   const [isDarkMode, toggleDarkMode] = useToggle(false);
   const [isEditMode, toggleEditMode] = useToggle(false);
 
@@ -12,14 +15,16 @@ function App() {
         isDarkMode ? "bg-gray-900 text-white" : "bg-purple-100"
       } p-4 sm:p-8`}
     >
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto flex flex-col min-h-screen">
         <Header
           isDarkMode={isDarkMode}
           isEditMode={isEditMode}
           toggleEditMode={toggleEditMode}
           toggleDarkMode={toggleDarkMode}
         />
-        <div className="flex min-h-screen items-center space-x-4"></div>
+        <div className="flex-1">
+          {habits.length === 0 ? <Welcome isDarkMode={isDarkMode} /> : null}
+        </div>
         <Footer isDarkMode={isDarkMode} />
       </div>
     </div>
