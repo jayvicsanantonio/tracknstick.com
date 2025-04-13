d7e2a397c09b27b1264be010f7758f64a32330c8
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-04-12 20:01:32 -0700

    Address comments

--- src/components/common/DailyHabitTracker.tsx
+++ src/components/common/DailyHabitTracker.tsx
@@ -7,9 +7,8 @@ import {
 } from 'react';
 import { useSWRConfig } from 'swr';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import { CalendarX2, PlusCircle } from 'lucide-react';
 import { Habit } from '@/types/habit';
+import NoHabits from '@/components/common/NoHabits';
 import DailyHabitDate from '@/components/common/DailyHabitDate';
 import DailyHabitProgressIndicator from '@/components/common/DailyHabitProgressIndicator';
 import DailyHabitList from '@/components/common/DailyHabitList';
@@ -137,44 +136,7 @@ export default function DailyHabitTracker({
         </CardHeader>
         <CardContent>
           {habits.length === 0 ? (
-            <div
-              className={`${
-                isDarkMode ? 'bg-gray-900' : 'bg-gray-100 '
-              } flex flex-col items-center justify-center gap-4 py-8 px-20 text-center rounded-lg`}
-            >
-              <CalendarX2
-                className={`${
-                  isDarkMode ? 'text-purple-400' : 'text-purple-600'
-                } h-12 w-12`}
-              />
-              <h3
-                className={`${
-                  isDarkMode ? 'text-purple-400' : 'text-purple-600'
-                } text-lg font-semibold`}
-              >
-                No Habits Found
-              </h3>
-              <p
-                className={`${
-                  isDarkMode ? 'text-purple-400' : 'text-purple-600'
-                } mt-2`}
-              >
-                Try adding a new habit to start tracking your daily
-                progress, or check a different date to view existing
-                habits. Building good habits is the first step towards
-                achieving your goals!
-              </p>
-              <Button
-                onClick={onAddHabitClick}
-                className={`${
-                  isDarkMode
-                    ? 'bg-purple-700 hover:bg-purple-600'
-                    : 'bg-purple-600 hover:bg-purple-700'
-                } text-white rounded-full font-semibold transition-colors duration-300 shadow-lg mt-4`}
-              >
-                <PlusCircle className="mr-2 h-4 w-4" /> Add New Habit
-              </Button>
-            </div>
+            <NoHabits onAddHabitClick={onAddHabitClick} />
           ) : (
             <>
               <DailyHabitProgressIndicator
