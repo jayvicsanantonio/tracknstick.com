22ef7218137a9dba4e57231d84ed6c9283c73a26
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-05-01 16:16:21 -0700

    Cleanup

--- src/components/common/HabitDialogBody.tsx
+++
@@ -1,7 +0,0 @@
-import { type ReactNode } from "react";
-
-export default function HabitDialogBody({ children }: { children: ReactNode }) {
-  return (
-    <div className="h-[608px] sm:h-[508px] grid gap-4 py-4">{children}</div>
-  );
-}
