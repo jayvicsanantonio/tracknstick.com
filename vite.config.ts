9fd20adfac3bc47c9ee72ff9c3280684ff4f6d84
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-12-09 16:24:31 -0800

    Remove Knip lintignore tag and PWA development mode setting.

--- vite.config.ts
+++ vite.config.ts
@@ -67,7 +67,6 @@ export default defineConfig({
           },
         ],
         inlineWorkboxRuntime: true,
-        mode: 'development',
       },
       strategies: 'generateSW',
       filename: 'sw.js',
